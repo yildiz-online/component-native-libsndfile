8fc07f464202a4953678dfb2dd32bad0a68f7880
Grégory Van den Borre <vandenborre.gregory@hotmail.fr>
2017-09-11 16:45:15 +0200

    [YIL-308] Improve cleaning script.

--- src/main/scripts/linux64/build.sh
+++ src/main/scripts/linux64/build.sh
@@ -1,10 +1,24 @@
 cmake ../../c++ -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX="../../../../target/classes/linux64" -G "Unix Makefiles"
 
 make install
+r1=$?
+make clean
 
 rm -R CMakeFiles
 rm CMakeCache.txt
 rm cmake_install.cmake
 rm Makefile
-rm test_physfs
 rm install_manifest.txt
+rm -r doc
+rm -r src
+rm -r Testing
+rm -r tests
+rm CPackConfig.cmake
+rm CPackSourceConfig.cmake
+rm CTestTestfile.cmake
+rm DartConfiguration.tcl
+rm LibSndFileConfig.cmake
+rm LibSndFileConfigVersion.cmake
+rm sndfile.pc
+
+return $r1
--- src/main/scripts/win64/build.sh
+++ src/main/scripts/win64/build.sh
@@ -1,9 +1,12 @@
 cmake ../../c++ -DCMAKE_BUILD_TYPE=Release -DCMAKE_COLOR_MAKEFILE=on -DCMAKE_INSTALL_PREFIX="../../../../target/classes/win64" -DCMAKE_TOOLCHAIN_FILE=mingw-toolchain.cmake
 
 make install
+r1=$?
 
 rm -R CMakeFiles
 rm CMakeCache.txt
 rm cmake_install.cmake
 rm Makefile
 rm install_manifest.txt
+
+return $r1
