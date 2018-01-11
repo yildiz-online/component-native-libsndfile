8d57aa0df032dc3306b8c24618aac361ca34196e
Grégory Van den Borre <vandenborre.gregory@hotmail.fr>
2018-01-11 17:07:29 +0100

    [YCMPSF-0] Remove win32.

--- src/main/scripts/linux64/build.sh
+++ src/main/scripts/linux64/build.sh
@@ -1,4 +1,13 @@
-cmake ../../c++ -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX="../../../../target/classes/linux64" -G "Unix Makefiles"
+cmake ../../c++ \
+-DCMAKE_BUILD_TYPE=Release \
+-DOGG_INCLUDE_DIR="ogg/linux64/include" \
+-DOGG_LIBRARY="ogg/linux64/lib/libogg.a" \
+-DVORBIS_INCLUDE_DIR="vorbis/linux64/include" \
+-DVORBIS_LIBRARY="vorbis/linux64/lib/libvorbis.a" \
+-DVORBISFILE_LIBRARY="vorbis/linux64/lib/libvorbisfile.a" \
+-DVORBISENC_LIBRARY="vorbis/linux64/lib/libvorbisenc.a"  \
+-DCMAKE_INSTALL_PREFIX="../../../../target/classes/linux64" \
+-G "Unix Makefiles"
 
 make install
 r1=$?
@@ -20,5 +29,7 @@ rm DartConfiguration.tcl
 rm LibSndFileConfig.cmake
 rm LibSndFileConfigVersion.cmake
 rm sndfile.pc
+rm -r ogg
+rm -r vorbis
 
 return $r1
--- src/main/scripts/win64/build.sh
+++ src/main/scripts/win64/build.sh
@@ -1,12 +1,34 @@
-cmake ../../c++ -DCMAKE_BUILD_TYPE=Release -DCMAKE_COLOR_MAKEFILE=on -DCMAKE_INSTALL_PREFIX="../../../../target/classes/win64" -DCMAKE_TOOLCHAIN_FILE=mingw-toolchain.cmake
+cmake ../../c++ \
+-DCMAKE_BUILD_TYPE=Release \
+-DBUILD_PROGRAMS=FALSE \
+-DBUILD_TESTING=FALSE \
+-DCMAKE_COLOR_MAKEFILE=on \
+-DOGG_INCLUDE_DIR="ogg/win64/include" \
+-DOGG_LIBRARY="ogg/win64/lib/libogg.a" \
+-DVORBIS_INCLUDE_DIR="vorbis/win64/include" \
+-DVORBIS_LIBRARY="vorbis/win64/lib/libvorbis.a" \
+-DVORBISFILE_LIBRARY="vorbis/win64/lib/libvorbisfile.a" \
+-DVORBISENC_LIBRARY="vorbis/win64/lib/libvorbisenc.a" \
+-DCMAKE_INSTALL_PREFIX="../../../../target/classes/win64" \
+-DCMAKE_TOOLCHAIN_FILE=mingw-toolchain.cmake
 
 make install
 r1=$?
+make clean
 
 rm -R CMakeFiles
 rm CMakeCache.txt
 rm cmake_install.cmake
 rm Makefile
 rm install_manifest.txt
+rm -r doc
+rm -r src
+rm CPackConfig.cmake
+rm CPackSourceConfig.cmake
+rm LibSndFileConfig.cmake
+rm LibSndFileConfigVersion.cmake
+rm sndfile.pc
+rm -r ogg
+rm -r vorbis
 
 return $r1
